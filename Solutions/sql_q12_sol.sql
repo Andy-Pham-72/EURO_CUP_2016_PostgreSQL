987d9767415ba9f6891ee67f61ad1128059cd6bf
Andy Pham <aqpham02@gmail.com>
2021-11-07 21:35:00 -0500

    small update

--- Solutions/sql_q12_sol.sql
+++ Solutions/sql_q12_sol.sql
@@ -1,9 +1,13 @@
--- Question 12: Write a SQL query that returns the total number of goals scored by each position on each country's team. Do not include positions which scored no goals.
+/* Question 12: Write a SQL query that returns the total 
+number of goals scored by each position on each country's team. 
+Do not include positions which scored no goals.
+*/
+
 SELECT s.country_name, p.posi_to_play, COUNT(*) AS total_score
 FROM euro_cup_2016.goal_details AS g
-LEFT JOIN euro_cup_2016.player_mast AS p
+JOIN euro_cup_2016.player_mast AS p
 	ON g.player_id= p.player_id
-LEFT JOIN euro_cup_2016.soccer_country AS s
+JOIN euro_cup_2016.soccer_country AS s
 	ON g.team_id= s.country_id
 GROUP BY s.country_name, p.posi_to_play
 ORDER BY s.country_name ;
